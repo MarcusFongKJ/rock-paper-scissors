f15f17cb51482c03e1dbe42d23af245467f722d5
MarcusFongKJ <65365866+MarcusFongKJ@users.noreply.github.com>
2023-09-06 00:15:45 +0800

    added function for user to play and restart game and view outcomes

--- javascript.js
+++ javascript.js
@@ -13,3 +13,85 @@ function getPlayerChoice() {
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     return playerSelection;
 }
+
+// Function to check if player choice is valid
+function validPlayerChoice() {
+    let choice = getPlayerChoice();
+    if (options.includes(choice)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Function to play the game
+function playRound(playerSelection, computerSelection) {
+    // Check Draw condition
+    if (playerSelection == computerSelection) {
+        return "Draw";
+    }
+
+    // Check Win condition (if not satisfied, game is lost)
+    if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return "Win";
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return "Win";
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return "Win";
+    } else {
+        return "Lose";
+    }
+}
+
+// Function to play game
+let playerWinCount = 0, computerWinCount = 0;
+function game() {  
+    // if player's choice is invalid, exit game
+    if (! validPlayerChoice()) {
+        alert("Enter a valid choice");
+        return;
+    }
+    let roundsList = document.getElementById("rounds");
+    let playerWin = document.getElementById("playerWinCount");
+    let computerWin = document.getElementById("computerWinCount");
+    let player = getPlayerChoice();
+    let computer = getComputerChoice();
+
+    if (playRound(player, computer) == "Win") {
+        let li = document.createElement('li');
+        li.innerText = `You Won! You played ${player} while Computer played ${computer}.`;
+        roundsList.appendChild(li);
+        playerWinCount++;
+    }  else if (playRound(player, computer) == "Lose") {
+        let li = document.createElement('li');
+        li.innerText = `You Lost! Computer played ${computer} while you played ${player}.`;
+        roundsList.appendChild(li);
+        computerWinCount++;
+    } else {
+        let li = document.createElement('li');
+        li.innerText = `You Drew! Both you and Computer played ${computer}.`;
+        roundsList.appendChild(li);
+    }
+
+    playerWin.innerHTML = playerWinCount;
+    computerWin.innerHTML = computerWinCount;
+}
+
+// Restart Game
+function restart() {
+    let roundsList = document.getElementById("rounds");
+    let playerWin = document.getElementById("playerWinCount");
+    let computerWin = document.getElementById("computerWinCount");
+    let playerSelection = document.getElementById("playerOption");
+
+    // Clear user input
+    playerSelection.value = "";
+
+    // Reset counter
+    playerWinCount = 0, computerWinCount = 0;
+    playerWin.innerHTML = playerWinCount;
+    computerWin.innerHTML = computerWinCount;
+
+    // Reset list
+    roundsList.innerHTML = "";
+}
