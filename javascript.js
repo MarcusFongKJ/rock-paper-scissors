49e8c5d9342f4114cc57385f7f557a0eca15a4f0
MarcusFongKJ <65365866+MarcusFongKJ@users.noreply.github.com>
2023-09-05 22:44:03 +0800

    added function to get player choice

--- javascript.js
+++ javascript.js
@@ -3,6 +3,13 @@ const options = ["Rock", "Paper", "Scissors"];
 
 // Function to get computer choice from options array
 function getComputerChoice() {
-    let computerChoice = options[Math.floor(Math.random() * options.length)];
-    return computerChoice;
+    let computerSelection = options[Math.floor(Math.random() * options.length)];
+    return computerSelection;
+}
+
+// Function to get player choice
+function getPlayerChoice() {
+    let playerSelection = document.getElementById("playerOption").value;
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    return playerSelection;
 }
